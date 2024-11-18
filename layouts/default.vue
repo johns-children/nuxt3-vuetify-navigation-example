e9bec4cac22e68d203f703f19220dd7ee5d4ec3e
johns-children <taotan421@gmail.com>
2024-11-18 17:24:54 +0900

    implement nav bar and page links

---
+++ layouts/default.vue
@@ -0,0 +1,38 @@
+<script setup>
+    const items = [
+        {
+        text: 'Home',
+        to: '/',
+        },
+        {
+        text: 'Test',
+        to: '/test',
+        }
+    ]
+
+    const route = useRoute()
+    const isActive = (path) => route.path === path
+
+</script>
+<template>
+    <div>
+        <v-layout>
+            <v-app-bar class="px-md-4">
+                <v-app-bar-title>Application</v-app-bar-title>
+                <NuxtLink
+                    v-for="(item, i) in items"
+                    :key="i"
+                    :to="item.to"
+                    class="mx-2 text-body-1 text-decoration-none"
+                    :class="isActive(item.to) ? `text-blue` : `text-grey-darken-4`"
+                >
+                    {{ item.text }}
+                </NuxtLink>
+            </v-app-bar>
+            <v-main>
+                <slot />
+            </v-main>
+        </v-layout>
+    </div>
+</template>
+  
