464b3639ceaf91a6788748ef094e397b44084fd5
johns-children <taotan421@gmail.com>
2024-11-18 21:33:32 +0900

    implement navigation drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -17,17 +17,21 @@
 <template>
     <div>
         <v-layout>
-            <v-app-bar class="px-md-4">
-                <v-app-bar-title>Application</v-app-bar-title>
-                <NuxtLink
-                    v-for="(item, i) in items"
-                    :key="i"
-                    :to="item.to"
-                    class="mx-2 text-body-1 text-decoration-none"
-                    :class="isActive(item.to) ? `text-blue` : `text-grey-darken-4`"
-                >
-                    {{ item.text }}
-                </NuxtLink>
+            <v-app-bar class="px-3">
+                <template v-slot:prepend>
+                    <v-app-bar-title>Application</v-app-bar-title>
+                    <div class="pl-10">
+                        <NuxtLink
+                            v-for="(item, i) in items"
+                            :key="i"
+                            :to="item.to"
+                            class="mx-2 text-body-1 text-decoration-none"
+                            :class="isActive(item.to) ? `text-blue` : `text-grey-darken-4`"
+                        >
+                            {{ item.text }}
+                        </NuxtLink>
+                    </div>
+                </template>
             </v-app-bar>
             <v-main>
                 <slot />
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,60 @@
+<script setup>
+    const drawer = ref(true)
+
+    const items = ref([
+        {
+            title: 'Dashboard',
+            prependIcon: 'mdi-view-dashboard-outline',
+            link: true,
+        },
+        {
+            title: 'Team',
+            prependIcon: 'mdi-account-group',
+            link: true,
+        },
+        {
+            title: 'Projects',
+            prependIcon: 'mdi-briefcase-outline',
+            link: true,
+        },
+        {
+            title: 'Calendar',
+            prependIcon: 'mdi-calendar',
+            link: true,
+        },
+        {
+            title: 'Reports',
+            prependIcon: 'mdi-file-chart-outline',
+            link: true,
+        },
+    ]);
+
+    function controlDrawer(drawerStatus) {
+        drawer.value = drawerStatus;
+    }
+
+</script>
+
 <template>
-    <h1>Home page</h1>
+    <v-app>
+        <v-layout>
+            <v-navigation-drawer
+                v-model="drawer"
+                permanent
+            >
+                <div class="pa-2">
+                    <v-btn icon="$vuetify" density="comfortable" variant="text" @click="controlDrawer(false)" />
+                </div>
+                <v-list density="compact" item-props :items="items" nav />
+            </v-navigation-drawer>
+            <v-main>
+                <v-sheet>
+                    <div class="pa-2" style="height: 64px">
+                        <v-btn v-show="!drawer" icon="$vuetify" density="comfortable" variant="text" @click="controlDrawer(true)" />
+                    </div>
+                    <div>main main main</div>
+                </v-sheet>
+            </v-main>
+        </v-layout>
+    </v-app>
 </template>
